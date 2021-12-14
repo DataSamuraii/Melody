14f9262f33f2916455b85fd58a9130022cc3c9bb
DataSamuraii <80856330+DataSamuraii@users.noreply.github.com>
2021-12-14 15:01:46 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ $(document).ready(function () {
     let counterDown = $(".counter_down"); /* кнопка уменьшениея этажа */
     let floorPath = $(".main-image path"); /* подсветка этажа дома в СВГ */
 
+    let modal = $(".modal");
+    let modalCloseButton = $(".modal-close-button");
+    let viewFlatsButton = $(".view-flats");
+    let currentFlat = 1;
+    let flatPath = $(".modal-image path");
+    let flatList = $(".flat-list a");
+
     function Count() {
         let usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false}) /* форматируем переменную с этажем, чтобы было 01 */
         $(".counter").text(usCurrentFloor); /* */
@@ -14,7 +21,7 @@ $(document).ready(function () {
     floorPath.on("mouseover", function () {
         floorPath.removeClass("current-floor");
         currentFloor = $(this).attr("data-floor"); /* получаем значение текужего этажа */
-        $(".counter").text(currentFloor); /* записываем знпчение этажа в счетчкик */
+        $(".counter").text(currentFloor); /* записываем знпчение этажа в счетчик */
     });
 
     /* отследить клик по кнопке вверх*/
@@ -31,5 +38,25 @@ $(document).ready(function () {
             currentFloor--;
             Count();
         }
+    });
+
+    floorPath.on("click", toggleModal);
+    modalCloseButton.on("click", toggleModal);
+    viewFlatsButton.on("click", toggleModal)
+    function toggleModal() {
+        modal.toggleClass("is-open")
+    }
+
+    flatPath.on("mouseover", function () {
+        $(".current-flat").removeClass("current-flat");
+        currentFlat = $(this).attr("data-flat");
+        $(`[data-flat=${currentFlat}]`).toggleClass("current-flat");
+    });
+
+    flatList.on("mouseover", function () {
+        $(".current-flat").removeClass("current-flat");
+        currentFlat = $(this).attr("data-flat");
+        console.log(currentFlat)
+        $(`[data-flat=${currentFlat}]`).toggleClass("current-flat");
     });
 });
