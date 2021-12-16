97da451f3ce6ad139f7a4df5157e3bc23d083331
DataSamuraii <80856330+DataSamuraii@users.noreply.github.com>
2021-12-16 21:24:19 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,27 +1,62 @@
-let kbd = document.getElementsByClassName("kbd");
+$(document).ready(function () {
+    let currentFloor = 2; /* переменная текущего этажа */
+    let counterUp = $(".counter_up"); /* кнопка увеличения этажа */
+    let counterDown = $(".counter_down"); /* кнопка уменьшениея этажа */
+    let floorPath = $(".main-image path"); /* подсветка этажа дома в СВГ */
 
-for (let i = 0; i < kbd.length; i++){
-    let keyLetter = kbd[i].innerHTML;
-    kbd[i].addEventListener("click", function (){
-        let audio = new Audio("sounds/" + keyLetter + ".mp3");
-        audio.play();
-        kbd[i].classList.add('slide-bottom');
-        kbd[i].style.animation = 'none';
-        kbd[i].offsetHeight; /* trigger reflow */
-        kbd[i].style.animation = null;
+    let modal = $(".modal");
+    let modalCloseButton = $(".modal-close-button");
+    let viewFlatsButton = $(".view-flats");
+    let currentFlat = 1;
+    let flatPath = $(".modal-image path");
+    let flatList = $(".flat-list a");
+
+    function Count() {
+        let usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false}) /* форматируем переменную с этажем, чтобы было 01 */
+        $(".counter").text(usCurrentFloor); /* */
+        floorPath.removeClass("current-floor"); /* удаляем активный класс этажа */
+        $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); /* подсветить этаж */
+    }
+
+    floorPath.on("mouseover", function () {
+        floorPath.removeClass("current-floor");
+        currentFloor = $(this).attr("data-floor"); /* получаем значение текужего этажа */
+        $(".counter").text(currentFloor); /* записываем знпчение этажа в счетчик */
     });
-}
-for (let i = 0; i < kbd.length; i++) {
-    let keyLetter = kbd[i].innerHTML;
-    document.addEventListener("keydown", function (event) {
-        if (event.code === "Key" + keyLetter) {
-            let keyLetter = kbd[i].innerHTML;
-            let audio = new Audio("sounds/" + keyLetter + ".mp3");
-            audio.play();
-            kbd[i].classList.add('slide-bottom');
-            kbd[i].style.animation = 'none';
-            kbd[i].offsetHeight; /* trigger reflow */
-            kbd[i].style.animation = null;
+
+    /* отследить клик по кнопке вверх*/
+    counterUp.on("click", function () {
+        if (currentFloor < 18) {
+            currentFloor++;
+            Count();
         }
-    })
-}
+    });
+
+    /* отследить клик по кнопке вниз*/
+    counterDown.on("click", function () {
+        if (currentFloor > 2) {
+            currentFloor--;
+            Count();
+        }
+    });
+
+    floorPath.on("click", toggleModal);
+    modalCloseButton.on("click", toggleModal);
+    viewFlatsButton.on("click", toggleModal)
+    function toggleModal() {
+        modal.toggleClass("is-open")
+    }
+
+    flatPath.on("mouseover", function () {
+        $(".current-flat").removeClass("current-flat");
+        currentFlat = $(this).attr("data-flat");
+        $(`[data-flat=${currentFlat}]`).toggleClass("current-flat");
+    });
+
+    flatList.on("mouseover", function () {
+        $(".current-flat").removeClass("current-flat");
+        currentFlat = $(this).attr("data-flat");
+        console.log(currentFlat)
+        $(`[data-flat=${currentFlat}]`).toggleClass("current-flat");
+    });
+});
